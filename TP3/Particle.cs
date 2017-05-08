76a8b167956b8cbff0f0610281a6dde87d90feb8
smasson99 <smasson99@gmail.com>
2017-05-07 20:11:23 -0400

    pp

---
+++ TP3/Particle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+using SFML.Graphics;
+
+namespace TP3
+{
+  public class Particle:Movable
+  {
+    //Propriétés
+
+    //Méthodes
+    public Particle(float posX, float posY, uint nbVertices, Color color, float dimension)
+    :base(posX, posY, nbVertices, color, 0.00f)
+    {
+      
+    }
+  }
+}
